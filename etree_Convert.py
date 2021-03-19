53b61ce26c33ea2c0b8840d3df7d0cdcb6d63178
lkprabha <prasliit18@gmail.com>
2021-03-19 16:02:51 +0800

    Etree exercise

---
+++ etree_Convert.py
@@ -0,0 +1,11 @@
+from lxml import etree
+
+class eTree_allocation:
+    def __init__(self, page_Source):
+        self.tree= etree.HTML(page_Source)
+
+    def raindiv(self):
+        locator = "//div[@class='right']/p[@class='panel-item']/span[@class='value']"
+        divs = self.tree.findall(locator)
+        return divs
+
